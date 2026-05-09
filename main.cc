d7f9ea662910d3d7beb736a6b7c4d57d871d5998
aaron <aaron.a@me.com>
2026-05-08 23:47:35 -0500

    starting project

---
+++ main.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include "vec3.h"
+#include "color.h"
+
+int main() {
+    // image
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render 
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    for (int j = 0; j < image_width; j++) {
+        for (int i = 0; i < image_height; i++) {
+            auto pixel_color = color(double(i)/(image_width-1), 0.0, double(j)/(image_height-1));
+            write_color(std::cout, pixel_color);
+        }
+    }
+}
