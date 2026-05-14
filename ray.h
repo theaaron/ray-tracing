9949608b9f434d10683d555730e5bb463b7a4938
aaron <aaron.a@me.com>
2026-05-13 22:09:30 -0500

    fixed const issue

--- ray.h
+++ ray.h
@@ -8,8 +8,8 @@ class ray {
         ray() {}
         ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
 
-        const point3& origin() { return orig; }
-        const vec3& direction() { return dir; }
+        const point3& origin() const { return orig; }
+        const vec3& direction() const { return dir; }
 
         point3 at(double t) const {
             return orig + (t*dir);
